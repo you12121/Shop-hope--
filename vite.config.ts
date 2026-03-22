7066151cb513326bb3601738911c6b1705168b4a
you12121 <168900436+you12121@users.noreply.github.com>
2026-03-22 09:20:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Shop-hope--/", // <- مهم لGitHub Pages
   server: {
     host: "::",
     port: 8080,
